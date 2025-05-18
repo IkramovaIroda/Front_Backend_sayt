ab41a55a7127157bac91b9b55e52cf7ac80a47f2
Ikramova Iroda <ikramovairoda1311@gmail.com>
2025-05-18 19:12:38 +0500

    commit

---
+++ src/main/java/org/example/sayt/SaytApplication.java
@@ -0,0 +1,13 @@
+package org.example.sayt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaytApplication.class, args);
+    }
+
+}
